8a424801bd83cc535a94a35cb81c099a0252fa10
oscar-quiroz <kairox.rt@gmail.com>
2021-11-03 11:32:12 -0500

    trabajando con un modelo real

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CubeComponent } from './cube/cube.component';
+import { ModeloComponent } from './modelo/modelo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CubeComponent,
+    ModeloComponent
   ],
   imports: [
     BrowserModule
